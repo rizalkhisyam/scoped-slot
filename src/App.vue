ec2e6848d3d3447ae93de35085f4d4a637ec0cee
rizalkhisyam <162410101048@cs.unej.ac.id>
2021-10-28 16:47:36 +0700

    add keep alive components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -9,24 +9,33 @@
     ></user-info>
     <course-goals #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
-    </course-goals>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ManageGoals,
+    ActiveGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: "Rizal Khisyam",
         description: "Site owner and admin",
@@ -34,6 +43,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp
+    }
+  }
 };
 </script>
 
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,42 @@
+<template>
+    <base-card>
+
+            <h2>Manage Goals</h2>
+            <input type="text" ref="goal">
+            <button @click="setGoal">Set Goal</button>
+            <teleport to="body">
+                <error-alert v-if="inputIsInvalid">
+                    <h2>Input is invalid!</h2>
+                    <p>Please enter at least 1 character</p>
+                    <button @click="confirmDialog">Oke</button>
+                </error-alert>
+            </teleport>
+
+    </base-card>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+    components: {
+        ErrorAlert
+    },
+    data(){
+        return {
+            inputIsInvalid: false
+        }
+    },
+    methods: {
+        setGoal(){
+            const eneteredValue = this.$refs.goal.value
+            if(eneteredValue === ""){
+                this.inputIsInvalid = true
+            }
+        },
+
+        confirmDialog(){
+            this.inputIsInvalid = false
+        }
+    }
+}
+</script>
